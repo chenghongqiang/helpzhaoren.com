2a4895ee766691e534fa90e11919a5551c802228
chenghongqiang <chenghongqiang123@yeah.net>
2017-12-27 09:49:51 +0800

    prod conf

--- public/init.php
+++ public/init.php
@@ -4,7 +4,7 @@
  */
 
 //定义项目环境
-defined('API_ENV') or define('API_ENV', 'dev');
+defined('API_ENV') or define('API_ENV', 'prod');
 
 // 定义项目路径
 defined('API_ROOT') || define('API_ROOT', dirname(__FILE__) . '/..');

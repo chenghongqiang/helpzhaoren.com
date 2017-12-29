fc30ff1d4a9a6e729c648322bfb4a9d21d693eeb
程宏强 <kewin.cheng@jinfuzi.com>
2017-12-29 16:45:39 +0800

    config modify

--- data/phalapi.sql
+++ data/phalapi.sql
@@ -21,7 +21,7 @@ CREATE TABLE `phal_user` (
 #用户钱包提现记录表
 CREATE TABLE `phal_user_wallet_record` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `openid` VARCHAR(50) NOT NULL COMMENT '用户openid',
+  `openId` VARCHAR(50) NOT NULL COMMENT '用户openid',
 
   `money` DECIMAL(5, 1) DEFAULT 0 COMMENT '本次提现金额',
   `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -32,7 +32,7 @@ CREATE TABLE `phal_user_wallet_record` (
 #用户找人记录表
 CREATE TABLE `phal_oper_record` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `openid` VARCHAR(50) NOT NULL COMMENT '用户openid',
+  `openId` VARCHAR(50) NOT NULL COMMENT '用户openid',
   `wx_self_code` VARCHAR(50) NOT NULL COMMENT '发起人微信号',
   `money` DECIMAL(5, 1) NOT NULL COMMENT '红包金额',
   `intro` VARCHAR(200) NOT NULL COMMENT '找人描述',
@@ -42,11 +42,22 @@ CREATE TABLE `phal_oper_record` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-#引荐记录表 被引荐人只会有一个
+#引荐人记录表
 CREATE TABLE `phal_intro_record` (
   `id` int(10) NOT NULL COMMENT '找人记录表id',
-  `openid` VARCHAR(50) NOT NULL COMMENT '用户openid',
-  `wx_introducers_code` VARCHAR(50) NOT NULL COMMENT '发起人微信号',
+  `openId` VARCHAR(50) NOT NULL COMMENT '用户openid',
+  `wx_introducer_code` VARCHAR(50) NOT NULL COMMENT '引荐人微信号',
+  `intro_state` tinyint(4) DEFAULT 1 COMMENT '引荐者所属人  1.引荐人',
+  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
+
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+#引荐被引荐成功表 引荐人、被引荐人只会有一个
+#考虑引荐人很多，后期数量大，加了一个引荐人记录表和引荐成功表
+CREATE TABLE `phal_intro_success_record` (
+  `id` int(10) NOT NULL COMMENT '找人记录表id',
+  `openId` VARCHAR(50) NOT NULL COMMENT '用户openid',
+  `wx_introducer_code` VARCHAR(50) NOT NULL COMMENT '引荐人微信号',
   `intro_state` tinyint(4) DEFAULT NULL COMMENT '引荐者所属人  1.引荐人 2.被引荐人',
   `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
 
@@ -55,14 +66,13 @@ CREATE TABLE `phal_intro_record` (
 #交易记录
 CREATE TABLE `phal_trade_record` (
   `id` int(10) NOT NULL COMMENT '交易记录表id',
-  `openid` VARCHAR(50) NOT NULL COMMENT '用户openid',
+  `openId` VARCHAR(50) NOT NULL COMMENT '用户openid',
   `out_trade_no` VARCHAR(32) NOT NULL COMMENT '商户订单号',
   `transaction_id` VARCHAR(32) NOT NULL COMMENT '微信支付订单号',
   `spbill_create_ip` VARCHAR(16) NOT NULL COMMENT '终端IP',
   `total_fee` int NOT NULL COMMENT '订单总金额，单位为分',
   `trade_type` VARCHAR(16) NOT NULL COMMENT '交易类型 JSAPI、NATIVE、APP',
   `time_end` VARCHAR(14) NOT NULL COMMENT '支付完成时间 yyyyMMddHHmmss'
-
 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 

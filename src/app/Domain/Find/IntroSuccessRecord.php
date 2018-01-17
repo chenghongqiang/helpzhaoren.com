8fd58ce808ed12d4f55eb1a00ff490308dce67b1
程宏强 <kewin.cheng@jinfuzi.com>
2018-01-17 15:39:01 +0800

    module

--- src/app/Domain/Find/IntroSuccessRecord.php
+++ src/app/Domain/Find/IntroSuccessRecord.php
@@ -68,7 +68,7 @@ class IntroSuccessRecord {
         try{
             $this->sendModuleMsg($dataParam);
         }catch (Exception $e) {
-            \PhalApi\DI()->logger->error(__CLASS__.__FUNCTION__, "发送模板消息失败，id:" . $record['id'],'errMsg:'. $e->getMessage());
+            \PhalApi\DI()->logger->error(__CLASS__.__FUNCTION__, "发送模板消息失败，id:" . $record['id']. " errMsg:". $e->getMessage());
         }
 
     }
